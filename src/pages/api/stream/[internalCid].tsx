c305c1b7777abeea5af380357f170cf488247273
Deki Akbar <itsdekiakbar@gmail.com>
2024-01-31 03:50:03 +0700

    API proxy for streaming file

---
+++ src/pages/api/stream/[internalCid].tsx
@@ -0,0 +1,40 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import {
+  BadRequestException,
+  MethodNotAllowedException,
+} from "@/utils/http-api-exception";
+import httpProxyMiddleware from "next-http-proxy-middleware";
+
+export default async function stream(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const requestMethod = req.method;
+  const { internalCid } = req.query;
+  if (!internalCid) {
+    BadRequestException(res);
+  }
+
+  const backendStreamUrl = `view/${internalCid}`;
+
+  switch (requestMethod) {
+    case "GET":
+      return httpProxyMiddleware(req, res, {
+        target: process.env.NEXT_PUBLIC_API_URL,
+        pathRewrite: [
+          {
+            patternStr: `api/stream/${internalCid}`,
+            replaceStr: backendStreamUrl,
+          },
+        ],
+      });
+    default:
+      MethodNotAllowedException(res);
+  }
+}
+
+export const config = {
+  api: {
+    responseLimit: false,
+  },
+};
