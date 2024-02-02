5973d97e850d0f536b42457f010ff48c06820f4d
Deki Akbar <itsdekiakbar@gmail.com>
2024-02-02 13:45:29 +0700

    update icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import MainLayout from "@/components/web/main-layout";
+import { signIn } from "next-auth/react";
+import { FaGoogle } from "react-icons/fa";
+import { HiCheckCircle } from "react-icons/hi";
 
 export default function Home() {
   return (
@@ -16,7 +19,6 @@ export default function Home() {
         <div className="relative z-10">
           <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10 lg:py-16">
             <div className="max-w-2xl text-center mx-auto">
-
               <div className="mt-5 max-w-2xl">
                 <h1 className="block font-semibold text-gray-800 text-3xl md:text-4xl lg:text-5xl dark:text-gray-200">
                   Decentralized Storage Based On IPFS
@@ -36,18 +38,10 @@ export default function Home() {
                 <a
                   className="inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 hover:from-violet-600 hover:to-blue-600 border border-transparent text-white text-sm font-medium rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white py-3 px-4 dark:focus:ring-offset-gray-800"
                   href="#"
+                  onClick={() => signIn()}
                 >
-                  <svg
-                    className="flex-shrink-0 w-4.5 h-4.5"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
-                  </svg>
-                  Continue with Github
+                  <FaGoogle size={20} />
+                  Continue with Google
                 </a>
               </div>
             </div>
@@ -73,69 +67,25 @@ export default function Home() {
                 className="pt-8 space-y-5 border-t border-gray-200 my-7 dark:border-gray-700"
               >
                 <li className="flex space-x-3">
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
+                  <HiCheckCircle className="text-purple-500" />
                   <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">
                     IPFS-Powered Uploads
                   </span>
                 </li>
                 <li className="flex space-x-3">
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
+                  <HiCheckCircle className="text-purple-500" />
                   <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">
                     Reliable IPFS Infrastructure
                   </span>
                 </li>
                 <li className="flex space-x-3">
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
+                  <HiCheckCircle className="text-purple-500" />
                   <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">
                     Cross-Browser Compatibility
                   </span>
                 </li>
                 <li className="flex space-x-3">
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
+                  <HiCheckCircle className="text-purple-500" />
                   <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">
                     Instant Shareable Links
                   </span>
@@ -169,86 +119,31 @@ export default function Home() {
                 className="pt-8 space-y-5 border-t border-gray-200 my-7 dark:border-gray-700"
               >
                 <li className="flex space-x-3">
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
+                  <HiCheckCircle className="text-purple-500" />
                   <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">
                     Effortless Video Streaming
                   </span>
                 </li>
                 <li className="flex space-x-3">
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
+                  <HiCheckCircle className="text-purple-500" />
                   <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">
                     Low Latency Streaming
                   </span>
                 </li>
                 <li className="flex space-x-3">
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
+                  <HiCheckCircle className="text-purple-500" />
                   <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">
                     Dynamic File Loading
                   </span>
                 </li>
                 <li className="flex space-x-3">
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
+                  <HiCheckCircle className="text-purple-500" />
                   <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">
                     Instant Preview Functionality
                   </span>
                 </li>
                 <li className="flex space-x-3">
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
+                  <HiCheckCircle className="text-purple-500" />
                   <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">
                     Secure Streaming Protocol
                   </span>
